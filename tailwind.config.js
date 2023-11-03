f03a2066ba58e13d2bdd671871f1719225446979
yaoska telleria <yaoskatelleria59@gmail.com>
2023-11-02 18:43:47 -0600

    Inicializacion del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    backgroundColor:  theme => ({ ...theme('colors'), 
+        'primary': '#CC2D4A', 
+        'secondary': '#8FA206', 
+        'tertiary': '#61AEC9', 
+      }),
+      // textColor:  theme => ({ ...theme('colors'), 
+      //   'primary': '#CC2D4A', 
+      //   'secondary': '#8FA206', 
+      //   'tertiary': '#61AEC9', 
+      // }),
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif'],
+      },
+    extend: {
+      backgroundImage: { 
+        'sanFrancisco': "url('./src/img/sanFrancisco.jpg')", 
+        'sanFranciscoDesktop': "url('./src/img/sanFranciscoDesktop.jpg')", 
+        'yosemite': "url('./src/img/yosemite.jpg')", 
+        'LA': "url('./src/img/LA.jpg')", 
+        'seattle': "url('./src/img/seattle.jpg')", 
+        'new_york': "url('./src/img/new_york.jpg')", 
+        'norway': "url('./src/img/norway.jpg')", 
+        'sydney': "url('./src/img/sydney.jpg')", 
+        'miami': "url('./src/img/miami.jpg')",
+        'switzerland': "url('./src/img/switzerland.jpg')", 
+        'bali': "url('./src/img/bali.jpg')", 
+        'chicago': "url('./src/img/chicago.jpg')", 
+        'europe': "url('./src/img/europe.jpg')", 
+        'iceland': "url('./src/img/iceland.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
